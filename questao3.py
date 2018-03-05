231ecbe37f4c8d4d6410946f827c55f66d1ab9a5
Luciano Filho <luciano00filho@gmail.com>
2018-03-05 01:13:12 -0300

    Resolução da questão 3. Tem que fazer a parte da comparação de strings.

---
+++ questao3.py
@@ -0,0 +1,22 @@
+#/usr/bin/python3.6
+
+aux, votacao, candidatos, limite, i = '',[],[],0,0
+
+print("Definição do pleito: ")
+limite = int(input())
+while (i < limite):
+	candidatos.append(input())
+	i += 1
+print()
+print("Votação:")
+while True:
+	aux = input()
+	if aux.lower() not in candidatos.lower():
+		break
+	else:
+		votacao.append(aux)
+print("Candidatos:",candidatos)
+print("Votação:",votacao)
+print()
+for nome in candidatos:
+	print("%s com %s voto(s)" % (nome,candidatos.lower().count(nome)))
