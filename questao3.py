85c548970ab66067e6c5ab27122b2ed0ab7b98a5
Luciano Filho <luciano.oliveira@odia.com.br>
2018-03-05 17:51:17 -0300

    Resolução da questão 3.

--- questao3.py
+++ questao3.py
@@ -1,29 +1,43 @@
 #/usr/bin/python3.6
+def procuraNome(nome,lista,contagem=False):
+	auxLista = []
+	
+	# criar a lista auxiliar
+	for n1 in lista:
+		auxLista.append(n1.lower())
 
-aux, votacao, candidatos, limite, i, parada = '', [], [], 0, 0, True
+	if (contagem): # faço a contagem dos votos
+		return auxLista.count(nome.lower())
+	else: # verifico se o nome votado é válido
+		if nome.lower() not in auxLista:
+			return False
+	
+	return True		
 
-print("Definição do pleito: ")
-limite = int(input())
-while (i < limite):
-	candidatos.append(input())
-	i += 1
-print()
-print("Votação:")
+def main():
+	aux, votacao, candidatos, limite, i, parada = '', [], [], 0, 0, True
 
-while True:
-	aux = input()
+	print("Definição do pleito:")
+	limite = int(input())
+	while (i < limite):
+		candidatos.append(input())
+		i += 1
+
+	print()
+	print("Votação:")
+	while (parada):
+		aux = input()
+		parada = procuraNome(aux,candidatos)
+		if (parada):
+			votacao.append(aux)
+		
+	# DEBUGGING
+	# print("Candidatos:",candidatos)
+	# print("Votação:",votacao)
+
+	print()
 	for nome in candidatos:
-		if nome.lower() != aux.lower():
-			parada = False
-		else:
-			parada = True
-		continue
-	if(parada):
-		break
-	else:
-		votacao.append(aux)
-print("Candidatos:",candidatos)
-print("Votação:",votacao)
-print()
-for nome in candidatos:
-	print("%s com 0 voto(s)" % (nome))
+		print("%s com %d voto(s)" % (nome,procuraNome(nome,votacao,True)))
+
+if __name__ == '__main__':
+	main()
