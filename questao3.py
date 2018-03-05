149373614c573c17cd37507b899a021305df0d2e
Luciano Filho <luciano.oliveira@odia.com.br>
2018-03-05 15:01:58 -0300

    Resolver contagem da votação.

--- questao3.py
+++ questao3.py
@@ -1,6 +1,6 @@
 #/usr/bin/python3.6
 
-aux, votacao, candidatos, limite, i, repetir = '', [], [], 0, 0, True
+aux, votacao, candidatos, limite, i, parada = '', [], [], 0, 0, True
 
 print("Definição do pleito: ")
 limite = int(input())
@@ -10,11 +10,20 @@ while (i < limite):
 print()
 print("Votação:")
 
-while (repetir):
-
-
+while True:
+	aux = input()
+	for nome in candidatos:
+		if nome.lower() != aux.lower():
+			parada = False
+		else:
+			parada = True
+		continue
+	if(parada):
+		break
+	else:
+		votacao.append(aux)
 print("Candidatos:",candidatos)
 print("Votação:",votacao)
 print()
 for nome in candidatos:
-	print("%s com 0 voto(s)" % (nome)
+	print("%s com 0 voto(s)" % (nome))
